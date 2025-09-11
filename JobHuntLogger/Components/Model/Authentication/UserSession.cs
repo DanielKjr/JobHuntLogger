d2c2fe8fd94f9b98fd4e6d2a85aa0978ad98379b
DanielKjr <danielkjrjensen@gmail.com>
2025-09-11 14:35:18 +0200

    Blank slate

---
+++ JobHuntLogger/Components/Model/Authentication/UserSession.cs
@@ -0,0 +1,28 @@
+﻿namespace JobHuntLogger.Components.Model.Authentication
+{
+
+	public class UserSession
+	{
+		public event Action? OnChange;
+
+		private bool isLoggedIn;
+		public bool IsLoggedin
+		{
+			get => isLoggedIn;
+			set
+			{
+				if (isLoggedIn != value)
+				{
+					isLoggedIn = value;
+					NotifyStateChanged();
+				}
+			}
+
+		}
+
+		public string UserName { get; set; } = string.Empty;
+		public string Role { get; set; } = string.Empty;
+
+		private void NotifyStateChanged() => OnChange?.Invoke();
+	}
+}
--- JobHuntLogger/Program.cs
+++ JobHuntLogger/Program.cs
@@ -1,11 +1,27 @@
 using JobHuntLogger.Components;
+using JobHuntLogger.Components.Model.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
+StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+builder.Configuration
+	.AddJsonFile("/run/secrets/dbinfo", optional: true, reloadOnChange: false);
 
+builder.Services.AddAuthenticationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<UserSession>();
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+   .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("Entra:Blazor"))
+		.EnableTokenAcquisitionToCallDownstreamApi()
+   .AddDownstreamApi("DownstreamApi", builder.Configuration.GetSection("Entra:JobHuntApi"))
+	.AddDistributedTokenCaches();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +33,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode();
 
