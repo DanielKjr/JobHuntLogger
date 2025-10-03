98df9b5b0531ae76cb8213315660dc79767b4fb4
DanielKjr <danielkjrjensen@gmail.com>
2025-10-03 14:15:47 +0200

    Distributed SQL server cache

--- JobHuntLogger/Program.cs
+++ JobHuntLogger/Program.cs
@@ -1,42 +1,45 @@
 using JobHuntLogger.Components;
 using JobHuntLogger.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
-
-
 builder.Services.AddRazorComponents(options =>
 	options.DetailedErrors = builder.Environment.IsDevelopment()).AddInteractiveServerComponents();
 
+
 builder.Services.AddControllersWithViews();
 StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+
+//Adding the compose secrets json as a configuration source
 //builder.Configuration
 //.AddJsonFile("/run/secrets/dbinfo", optional: true, reloadOnChange: true);
 
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddTransient<JobHuntApiService>();
-builder.Services.AddHttpClient();
 
+builder.Services.AddHttpClient();
+var Configuration = builder.Configuration;
+
+//Entra with SQL distributed token cache
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+	options.ConnectionString = Configuration.GetConnectionString("TokenDb");
+	options.SchemaName = "Tokens";
+	options.TableName = "TokenCache";
+});
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 	.AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("Entra:Blazor"))
 	.EnableTokenAcquisitionToCallDownstreamApi(new[] { "User.Read" })
-	.AddMicrosoftGraph()
-	.AddInMemoryTokenCaches();
-builder.Services.Configure<CookieAuthenticationOptions>(options =>
-{
-	options.Cookie.Name = "PersistentLogin";
-
-	options.ExpireTimeSpan = TimeSpan.FromDays(14); // Persistent for 14 days
-	options.SlidingExpiration = true;
-});
+	.AddMicrosoftGraph().
+	AddDistributedTokenCaches();
 
 
 builder.Services.AddAuthorization();
 builder.Services.AddCascadingAuthenticationState();
 
+//Optional enforce authorize on all pages
 //builder.Services.AddAuthorizationBuilder()
 //	.SetFallbackPolicy(new AuthorizationPolicyBuilder()
 //		.RequireAuthenticatedUser()
